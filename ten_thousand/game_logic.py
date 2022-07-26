b8b59025a10ce4f14ecd03ddc6db2a59a799af83
Benjamin Small <smallbenjamin94@gmail.com>
2022-07-25 22:40:17 -0700

    added full house functionality

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -14,32 +14,55 @@ class GameLogic:
     def calculate_score(selected_dice):
         shelf_score = 0
         rolled_num = 1
-        rolled_nums_count = {}
+        selected_dice_count = {}
         while rolled_num <= 6:
             if rolled_num == 1:
                 ones_count = selected_dice.count(1)
                 if ones_count:
-                    rolled_nums_count[1] = ones_count
-                    if rolled_nums_count[1] <= 2:
-                        shelf_score += 100 * rolled_nums_count[1]
+                    selected_dice_count[1] = ones_count
+                    if selected_dice_count[1] <= 2:
+                        shelf_score += 100 * selected_dice_count[1]
                     else:
-                        shelf_score += 1000 * (rolled_nums_count[1] - 2)
+                        shelf_score += 1000 * (selected_dice_count[1] - 2)
             elif rolled_num == 5 and 1 <= selected_dice.count(5) <= 2:
-                rolled_nums_count[5] = selected_dice.count(5)
+                selected_dice_count[5] = selected_dice.count(5)
                 shelf_score += 50 * selected_dice.count(5)
             else:
                 roll_count = selected_dice.count(rolled_num)
                 if roll_count:
-                    rolled_nums_count[rolled_num] = roll_count
-                    if rolled_nums_count[rolled_num] > 2:
-                        shelf_score += rolled_num * 100 * (rolled_nums_count[rolled_num] - 2)
+                    selected_dice_count[rolled_num] = roll_count
+                    if selected_dice_count[rolled_num] > 2:
+                        shelf_score += rolled_num * 100 * (selected_dice_count[rolled_num] - 2)
             rolled_num += 1
-        if len(rolled_nums_count) == 6:
-            print(rolled_nums_count)
+        if len(selected_dice_count) == 6:
+            print(selected_dice_count)
             return 1500
-        if len(rolled_nums_count) == 3:
-            for num in rolled_nums_count.values():
-                if num != 2:
-                    break
-            return 1500
+        if len(selected_dice_count) == 3:
+            if GameLogic.double_trips(selected_dice_count):
+                return 1500
+        if GameLogic.is_full_house(selected_dice_count):
+            print(selected_dice_count)
+            if sum(selected_dice_count.values()) == 5:
+                return 1500
+            else:
+                if 1 in selected_dice_count and selected_dice_count[1] == 1:
+                    return 1600
+                elif 5 in selected_dice_count and selected_dice_count[5] == 1:
+                    return 1550
         return shelf_score
+
+    @staticmethod
+    def is_full_house(selected_dice_count):
+        full_house_criteria = {2: False, 3: False}
+        for count in selected_dice_count.values():
+            full_house_criteria[count] = True
+        if full_house_criteria[2] and full_house_criteria[3]:
+            return True
+        return False
+
+    @staticmethod
+    def double_trips(selected_dice_count):
+        for num in selected_dice_count.values():
+            if num != 2:
+                return False
+        return True
