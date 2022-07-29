14cf1c5191ad7ec0021f26af5d41a0d54604a93d
Benjamin Small <smallbenjamin94@gmail.com>
2022-07-28 20:15:01 -0700

    restored default behavior to play_game.py

--- ten_thousand/play_game.py
+++ ten_thousand/play_game.py
@@ -156,6 +156,6 @@ def bank_roll_or_quit():
 
 if __name__ == "__main__":
     if start_game():
-        play_game([(2, 3, 1, 3, 1, 2), (4, 1, 4, 4, 3, 4), (3, 2, 3, 2, 1, 4)])
+        play_game()
     else:
         print("OK. Maybe another time")
